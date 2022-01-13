b165c285726b807c80fd19fd0c6d440f6a572cfc
Martin Burolla <mburolla@xpanxion.com>
2022-01-13 11:15:00 -0500

    update

---
+++ readme.md
@@ -0,0 +1,24 @@
+# JavaScript 101
+JavaScript assignment repo
+
+# Assignments
+
+## Ex. 1
+
+## Ex. 2
+
+## Ex. 3
+
+## Ex. 4
+
+## Ex. 5
+
+## Ex. 6
+
+## Ex. 7
+
+## Ex. 8
+
+## Ex. 9
+
+## Ex. 10
---
+++ student7/app.js
@@ -0,0 +1,42 @@
+//
+// File: app.js
+// Date: 1/13/2022
+// 
+
+let ex1 = () => {
+    console.log('*** Ex. 1 ***')
+}
+
+let ex2 = () => {
+    console.log('*** Ex. 2 ***')
+}
+
+let ex4 = () => {
+    console.log('*** Ex. 4 ***')
+}
+
+let ex5 = () => {
+    console.log('*** Ex. 5 ***')
+}
+
+let ex6 = () => {
+    console.log('*** Ex. 6 ***')
+}
+
+let ex7 = () => {
+    console.log('*** Ex. 7 ***')
+}
+
+let ex8 = () => {
+    console.log('*** Ex. 8 ***')
+}
+
+let ex9 = () => {
+    console.log('*** Ex. 9 ***')
+}
+
+let ex10 = () => {
+    console.log('*** Ex. 10 ***')
+}
+
+ex1();
