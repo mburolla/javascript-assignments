ded17764ea3bea344c002573a6ad9572f800604e
Martin Burolla <mburolla@xpanxion.com>
2022-01-13 16:23:24 -0500

    ex1

--- instructor/app.js
+++ instructor/app.js
@@ -3,40 +3,70 @@
 // Date: 1/13/2022
 // 
 
+var readline = require('readline');
+
+// let rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+//   terminal: false
+// });
+
+// rl.on('line', (line) => {
+//     console.log(line);
+// });
+
 let ex1 = () => {
-    console.log('*** Ex. 1 ***')
+    console.log('*** Ex. 1 ***');
+
+    let dict = [
+        {"firstName": "joe", "lastName": "smith", age: 10},
+        {"firstName": "paul", "lastName": "simmon", age: 20},
+        {"firstName": "fred", "lastName": "jones", age: 30},
+    ]
+
+    let age = 0
+    for (let key in dict){
+        age += dict[key].age;
+    }
+      
+    console.log(`The total age is: ${age}.`);
 }
 
 let ex2 = () => {
-    console.log('*** Ex. 2 ***')
+    console.log('*** Ex. 2 ***');
 }
 
 let ex4 = () => {
-    console.log('*** Ex. 4 ***')
+    console.log('*** Ex. 4 ***');
 }
 
 let ex5 = () => {
-    console.log('*** Ex. 5 ***')
+    console.log('*** Ex. 5 ***');
 }
 
 let ex6 = () => {
-    console.log('*** Ex. 6 ***')
+    console.log('*** Ex. 6 ***');
 }
 
 let ex7 = () => {
-    console.log('*** Ex. 7 ***')
+    console.log('*** Ex. 7 ***');
 }
 
 let ex8 = () => {
-    console.log('*** Ex. 8 ***')
+    console.log('*** Ex. 8 ***');
 }
 
 let ex9 = () => {
-    console.log('*** Ex. 9 ***')
+    console.log('*** Ex. 9 ***');
 }
 
 let ex10 = () => {
-    console.log('*** Ex. 10 ***')
+    console.log('*** Ex. 10 ***');
 }
 
-ex1();
+const main = async () =>{
+    ex1();
+}
+
+main();
+
--- readme.md
+++ readme.md
@@ -5,6 +5,22 @@ JavaScript assignment repo
 
 ## Ex. 1
 
+Given the following dictionary:
+
+```
+    let dict = [
+        {"firstName": "joe", "lastName": "smith", age: 10},
+        {"firstName": "paul", "lastName": "simmon", age: 20},
+        {"firstName": "fred", "lastName": "jones", age: 30},
+    ]
+```
+
+Display the total age on the console:
+
+```
+The total age is: 60.
+```
+
 ## Ex. 2
 
 ## Ex. 3
