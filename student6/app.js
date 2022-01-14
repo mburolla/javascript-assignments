05066a67e88a773cde5cea3cc2eb6d9ddda43917
Michael Mayes <m0m0sxr@homeoffice.wal-mart.com>
2022-01-14 14:27:24 -0600

    commit ex 1 thru 5

--- student6/app.js
+++ student6/app.js
@@ -4,23 +4,59 @@
 // 
 
 let ex1 = () => {
-    console.log('*** Ex. 1 ***');
+    let dict = [
+        {"firstName": "joe", "lastName": "smith", age: 10},
+        {"firstName": "paul", "lastName": "simmon", age: 20},
+        {"firstName": "fred", "lastName": "jones", age: 30},
+    ]
+
+    const sumAge = dict.map(i => i.age).reduce((prev, curr) => prev + curr, 0);
+    console.log(`The total age is ${sumAge}`);
 }
 
 let ex2 = () => {
-    console.log('*** Ex. 2 ***');
+    let customers = [{
+        name: 'ABC Inc',
+        credit: 100
+    }, {
+        name: 'ACME Corp',
+        credit: 200
+    }, {
+        name: 'IoT AG',
+        credit: 300
+    }];
+
+    const customerList = customers.filter(customer => customer.credit === 200);
+    console.log(customerList);
 }
 
 let ex3 = () => {
-    console.log('*** Ex. 3 ***');
+    let str = "today this is a this is a this is a test.";
+    const strCounter = (words, find) => {
+        return (words.split(find)).length - 1;
+    }
+
+    const result = strCounter(str, "this");
+    console.log(`this appears: ${result} times`);
 }
 
 let ex4 = () => {
-    console.log('*** Ex. 4 ***');
+    let strArray = ['this', 'is', 'a', 'test', 'happy'];
+
+    const student6LongestString = strArray.reduce((prev, curr) => prev.length > curr.length ? prev : curr, '');
+    console.log(student6LongestString);
 }
 
 let ex5 = () => {
-    console.log('*** Ex. 5 ***');
+    const student6Palindrome = str => {
+        let reg = /[\W_]/g;
+        let lower = str.toLowerCase().replace(reg, '');
+        let reverse = lower.split('').reverse().join('');
+        return reverse === lower;
+    }
+
+    console.log(student6Palindrome('radar'));
+    console.log(student6Palindrome('month'));
 }
 
 let ex6 = () => {
@@ -44,11 +80,11 @@ let ex10 = () => {
 }
 
 const main = async () =>{
-     ex1();
+    // ex1();
     // ex2();
     // ex3();
     // ex4();
-    // ex5();
+     ex5();
     // ex6();
     // ex7();
     // ex8();
