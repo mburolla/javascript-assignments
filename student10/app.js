31a258a11c787648e39b3a7bffbf2254a64e41cd
Rachel Eckels <Rachel.Eckels@walmart.com>
2022-01-14 15:15:10 -0600

    Finish Exercises 1,2,3,4 and 5

--- student10/app.js
+++ student10/app.js
@@ -5,22 +5,51 @@
 
 let ex1 = () => {
     console.log('*** Ex. 1 ***');
+    let dict = [
+        {"firstName": "joe", "lastName": "smith", age: 10},
+        {"firstName": "paul", "lastName": "simmon", age: 20},
+        {"firstName": "fred", "lastName": "jones", age: 30},
+    ]
+    let totalAge=0;
+    let adding = dict.forEach(customers => totalAge += customers.age);
+    console.log('The total age is: ' + totalAge);
 }
 
 let ex2 = () => {
     console.log('*** Ex. 2 ***');
+    let customers = [{
+        name: 'ABC Inc',
+        credit: 100
+    }, {
+        name: 'ACME Corp',
+        credit: 200
+    }, {
+        name: 'IoT AG',
+        credit: 300
+    }];
+    let result = customers.filter(customers => customers.credit === 200);
+    console.log(result[0]);
 }
 
 let ex3 = () => {
     console.log('*** Ex. 3 ***');
+    let str = "today this is a this is a this is a test.";
+    let amount = str.match(/this/g).length;
+    console.log("this appears: " + amount + " times.");
 }
 
 let ex4 = () => {
     console.log('*** Ex. 4 ***');
+    array = ['this', 'is', 'a', 'test', 'happy'];
+    let longest = "";
+    console.log(longestString(array));
 }
 
 let ex5 = () => {
     console.log('*** Ex. 5 ***');
+    console.log(palindrome("radar"));
+    console.log(palindrome("month"));
+
 }
 
 let ex6 = () => {
@@ -44,11 +73,11 @@ let ex10 = () => {
 }
 
 const main = async () =>{
-     ex1();
-    // ex2();
-    // ex3();
-    // ex4();
-    // ex5();
+    //ex1();
+    //ex2();
+    //ex3();
+    //ex4();
+    ex5();
     // ex6();
     // ex7();
     // ex8();
@@ -59,7 +88,24 @@ const main = async () =>{
 //
 // Your functions here...
 //
+function palindrome(word){
+    let reverseWord = word.split("").reverse().join("");
+    if (word===reverseWord){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
+function longestString(array) {
+    for (let i=0; i<array.length; i++) {
+        if (array[i].length > longest.length) {
+            longest = array[i]
+        }
+    }
+    return longest;
+}
 
 
 //
